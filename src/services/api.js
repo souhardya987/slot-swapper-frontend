0842380a0af424807025c73ab3b151e6708d7363
Souhardya <sou.dutta.14@gmail.com>
2025-11-05 00:58:39 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5001/api', 
+  baseURL: 'https://slot-swapper-auu6.onrender.com/api', 
 });
 
 export const setAuthToken = (token) => {
